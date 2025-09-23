07f3d3a79c2ef5a6317d9cf02e78c86d2f1037e4
Alejandro del Valle Vallés <alejandrodelvalle06@outlook.es>
2025-09-23 18:01:03 +0200

    Termino la parte gráfica de la calculadora

--- Calculadora/app/src/main/java/com/alejandro/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/alejandro/calculadora/MainActivity.kt
@@ -25,7 +25,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    fun onClickedNumberButton(view: View) {
+    fun onClickedNumber(view: View) {
+
+    }
+
+    fun onClickedDecimal(view: View) {
+
+    }
+
+    fun onClickedPlus(view: View) {
+
+    }
+
+    fun onClickedMinus(view: View) {
+
+    }
+
+    fun onClickedMultiply(view: View) {
+
+    }
+
+    fun onClickedDivide(view: View) {
+
+    }
+
+    fun onClickedSquare(view: View) {
+
+    }
+
+    fun onClickedEqual(view: View) {
+
+    }
+
+    fun onClickedClear(view: View) {
+
+    }
+
+    fun onClickedDelete(view: View) {
+
+    }
+
+    fun onClickedChangeSymbol(view: View) {
 
     }
 }
