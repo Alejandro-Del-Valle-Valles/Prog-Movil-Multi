1d084eadf1dc6748ea1a1b10d0383f88c5344557
Alejandro del Valle Vallés <alejandrodelvalle06@outlook.es>
2025-12-11 18:00:23 +0100

    Termino el main y el menu principal

--- Trimestre-02/Paqueteria/app/src/main/java/com/alejandro/paqueteria/controller/MainActivity.kt
+++ Trimestre-02/Paqueteria/app/src/main/java/com/alejandro/paqueteria/controller/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.alejandro.paqueteria.controller
 
+import android.content.Intent
+import com.alejandro.paqueteria.R
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.alejandro.paqueteria.databinding.ActivityMainBinding
@@ -14,5 +18,34 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.SendPackage -> {
+                intent = Intent(this, PackageSenderActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.SendMilCertificated -> {
+                intent = Intent(this, LetterSenderActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.MailReception -> {
+                intent = Intent(this, ReceptionActivity::class.java)
+                intent.putExtra("IsFromMenu", true)
+                startActivity(intent)
+                return true
+            }
+            else -> return false
+        }
     }
 }
